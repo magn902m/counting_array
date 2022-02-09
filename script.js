c3e1769186e710708656d732bae36b0e187617ed
Magnus <magn902m@stud.kea.dk>
2022-02-09 14:30:01 +0100

    a working array counter with a len of 9

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 `use strict`;
 
-window.addEventListener("load", start);
+window.addEventListener("DOMContentLoaded", start);
+
+let i = 0;
+let maxLen = 10;
+let result = [];
 
 function start() {
   console.log("start");
+  initLoop();
+}
+
+function initLoop() {
+  console.log("initLoop");
+  loop();
+}
+
+function loop() {
+  //   console.log("loop");
+  i++;
+  if (i < maxLen) {
+    result.unshift(i);
+    console.log(result);
+    // console.log("I nr.:", i);
+    setTimeout(loop, 250);
+    // loop();
+  } else {
+    i - 2;
+    result.pop(i);
+    result.unshift(i);
+    console.log(result);
+    setTimeout(loop, 250);
+    // console.log("done");
+  }
 }
